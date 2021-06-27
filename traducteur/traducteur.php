3d15a60371d688b8c8772807f34bc50670a7bee2
azadeh <azadeh.amohammadi@gmail.com>
2021-06-27 06:16:46 +0200

    les chanement

---
+++ traducteur/traducteur.php
@@ -0,0 +1,30 @@
+<?
+$mots = array(
+  'bonjour' => array('en' => 'hello', 'es' => 'buenos dias'),
+  'merci' => array('en' => 'thank you', 'es' => 'gracias'),
+  'au revoir' => array('en' => 'goodbye', 'es' => 'adios'),
+  'droite' => array('en' => 'right', 'es' => 'derecha'),
+  'gauche' => array('en' => 'left', 'es' => 'izquierda'),
+  'haut' => array('en' => 'top', 'es' => 'superior'),
+  'bas' => array('en' => 'bottom', 'es' => 'bajo'),
+  'chien' => array('en' => 'dog', 'es' => 'perro'),
+  'chat' => array('en' => 'cat', 'es' => 'gato'),
+  'pomme' => array('en' => 'apple', 'es' => 'manzana'),
+  'bananne' => array('en' => 'banana', 'es' => 'platano')
+);
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Traducteur</title>
+</head>
+<body>
+<form method="POST"></form>
+<label>Ecrivez votre mot :</label>
+<input name="mot">
+  
+</body>
+</html>
